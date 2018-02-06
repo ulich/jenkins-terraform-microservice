96f623abcfaedf294c0356754909eb2136d1d297
Ulich, Timo <timo@ulich.net>
2018-02-06 23:14:17 +0100

    do not create eb app again on prod

--- infrastructure/modules/elasticbeanstalk/elasticbeanstalk.tf
+++ infrastructure/modules/elasticbeanstalk/elasticbeanstalk.tf
@@ -1,12 +1,13 @@
 # Please refer to https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/command-options-general.html
 
 resource "aws_elastic_beanstalk_application" "application" {
+  count = "${local.workspace["create_application"]}"
   name = "${var.app_name}"
 }
 
 resource "aws_elastic_beanstalk_environment" "environment" {
   name                = "${var.app_name}-${local.workspace["environment_name"]}"
-  application         = "${aws_elastic_beanstalk_application.application.name}"
+  application         = "${var.app_name}"
   solution_stack_name = "64bit Amazon Linux 2017.09 v2.6.5 running Java 8"
   tier                = "WebServer"
 
--- infrastructure/modules/elasticbeanstalk/locals.tf
+++ infrastructure/modules/elasticbeanstalk/locals.tf
@@ -2,9 +2,11 @@ locals {
   workspaces = {
     default = {
       environment_name = "staging"
+      create_application = true
     }
     prod = {
       environment_name = "prod"
+      create_application = false
     }
   }
   workspace  = "${local.workspaces[terraform.workspace]}"
